1c10e95e15a9607958445bd286b9103ed5028331
nachovrgs <ivargas@jaer.tech>
2018-10-07 16:04:28 -0300

    add gitignore

--- crawler.py
+++ crawler.py
@@ -3,17 +3,19 @@ from parsers import infocasas_parser
 from bs4 import BeautifulSoup
 import threading
 
-max_pages = 20
+max_pages = 1
 properties = []
 remainingPages = True
+
 
 def getPage(page):
     if(page == 1):
         url_suffix = '?&ordenListado=3'
     else:
         url_suffix = '("pagina" +str(page))?&ordenListado=3'
-    url = 'https://www.infocasas.com.uy/alquiler/inmuebles/montevideo/' + url_suffix
-    print('Getting: ' + url)
+    url = 'https://www.infocasas.com.uy/alquiler/inmuebles/montevideo/' + \
+        url_suffix
+    print('Getting page: ' + str(page))
     response = requests.get(url)
     if (response.status_code < 400):
         plain_txt = response.text
@@ -23,11 +25,15 @@ def getPage(page):
         remainingPages = False
         print("Got to the end")
 
+
 def main():
     page = 1
-    while (page < max_pages and remainingPages):
+    print('Getting {} pages from InfoCasas'.format(str(max_pages)))
+    while (page <= max_pages and remainingPages):
         threading.Thread(target=getPage(page)).start()
         page += 1
     print('Se obtuvieron {} propiedades'.format(len(properties)))
+
+
 if __name__ == '__main__':
     main()
--- domain/Property.py
+++ domain/Property.py
@@ -1,11 +1,11 @@
 class Property(object):
-    def __init__(self, name, price, zone, type, reference):
+    def __init__(self, name, price, zone, type, images, reference):
         self.name = name
         self.price = price
         self.zone = zone
         self.type = type
+        self.images = images
         self.reference = reference
-
 
     def __str__(self):
         rep = "Property:\n"
@@ -13,5 +13,8 @@ class Property(object):
         rep += "price: " + self.price.encode('utf-8').strip() + "\n"
         rep += "zone: " + self.zone.encode('utf-8').strip() + "\n"
         rep += "type: " + str(self.type).encode('utf-8').strip() + "\n"
+        rep += "images: " + \
+            (', '.join([str(image).encode('utf-8') for image in self.images]))\
+            + "\n"
         rep += "reference: " + self.reference.encode('utf-8').strip() + "\n"
         return rep
